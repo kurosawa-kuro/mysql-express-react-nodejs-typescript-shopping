cd794e187eee00fe82f6f6778ec530e4efe9f50a
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-06-09 09:46:40 +0900

    fix index

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,5 @@
-// import app from "./index.js"
+import app from "./index"
 
-// app.listen(5000, () => // Use the imported 'app' here
-//   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${5000}`)
-// );
-
-console.log("Hello World")
+app.listen(5000, () => // Use the imported 'app' here
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${5000}`)
+);
