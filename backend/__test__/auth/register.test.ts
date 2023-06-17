ad2efd4731e03751eafa3b8020c94bbf0148aaeb
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-06-17 20:38:39 +0900

    fix database

--- backend/__test__/auth/register.test.ts
+++ backend/__test__/auth/register.test.ts
@@ -2,7 +2,7 @@
 
 import request, { SuperAgentTest } from "supertest";
 import { app } from "../../index";
-import { clearDatabase, createUser, loginUserAndGetToken } from "../test-utils";
+import { clearDatabase, createUser } from "../test-utils";
 
 describe("POST /api/users/register", () => {
   let agent: SuperAgentTest;
--- backend/__test__/database/user.test.ts
+++ backend/__test__/database/user.test.ts
@@ -1,7 +1,7 @@
 // backend\test\app.test.ts
 
 // import request from "supertest";
-import { User } from "@prisma/client";
+import { Order, Product, User } from "@prisma/client";
 import { db } from "../.././database/prisma/prismaClient";
 import { clearDatabase } from ".././test-utils";
 import { hashPassword } from "../.././utils";
@@ -24,12 +24,74 @@ describe("Database Connection", () => {
 
     const user: User = await db.user.create({
       data: {
-        name: "Test User",
-        email: " ",
-        password: await hashPassword("TestUserPassword123"),
+        name: "john",
+        email: "john@email.com",
+        password: await hashPassword("123456"),
         isAdmin: false,
       },
     });
     console.log({ user });
+
+    const adminUser: User = await db.user.create({
+      data: {
+        name: "admin",
+        email: "admin@email.com",
+        password: await hashPassword("123456"),
+        isAdmin: true,
+      },
+    });
+    console.log({ adminUser });
+
+    const product: Product = await db.product.create({
+      data: {
+        name: "Airpods Wireless Bluetooth Headphones",
+        price: 100,
+        user: { connect: { id: Number(adminUser.id) } },
+        image: "/images/airpods.jpg",
+        brand: "Apple",
+        category: "Electronics",
+        countInStock: 100, // Change to variable
+        numReviews: 100, // Change to variable
+        description: "aaaa", // Add variable
+      },
+    });
+    console.log({ product });
+
+    const createdOrder: Order = await db.order.create({
+      data: {
+        userId: Number(user.id),
+        address: "123 Main Street",
+        city: "Boston",
+        postalCode: "12345",
+        paymentMethod: "paymentMethod",
+        itemsPrice: 100,
+        taxPrice: 100,
+        shippingPrice: 100,
+        totalPrice: 100,
+      },
+    });
+    console.log({ createdOrder });
+
+    const orderProduct = await db.orderProduct.create({
+      data: {
+        orderId: createdOrder.id,
+        productId: product.id,
+        qty: 1,
+      },
+    });
+    console.log({ orderProduct });
+
+    const orderProducts = await db.orderProduct.findMany({
+      where: { orderId: createdOrder.id },
+      include: {
+        product: true,
+        order: {
+          include: {
+            user: true,
+          },
+        },
+      },
+    });
+    console.dir(orderProducts, { depth: null });
   });
 });
