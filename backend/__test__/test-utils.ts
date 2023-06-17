b3b0343a188daedad15e10d721c81f5233f7dd60
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-06-17 15:08:40 +0900

    fix order

--- backend/__test__/test-utils.ts
+++ backend/__test__/test-utils.ts
@@ -8,6 +8,7 @@ import { User, Product } from "@prisma/client";
 import path from "path";
 import { hashPassword } from "../utils";
 
+// Database Operations
 export const clearDatabase = async (): Promise<void> => {
   await db.orderProduct.deleteMany();
   await db.product.deleteMany();
@@ -15,6 +16,23 @@ export const clearDatabase = async (): Promise<void> => {
   await db.user.deleteMany();
 };
 
+export const createProduct = async (userId: number): Promise<Product> => {
+  return await db.product.create({
+    data: {
+      userId,
+      name: "Test Product",
+      price: 100,
+      image: "sample path",
+      brand: "Test Brand",
+      category: "Test Category",
+      countInStock: 10,
+      numReviews: 0,
+      description: "Test Description",
+    },
+  });
+};
+
+// User Operations
 export const createUser = async (
   email: string,
   password: string
@@ -41,62 +59,6 @@ export const createAdminUser = async (
       email,
       password: hashedPassword,
       isAdmin: true,
-    },
-  });
-};
-
-export const loginUserAndGetToken = async (
-  agent: SuperAgentTest,
-  email: string,
-  password: string
-): Promise<string> => {
-  const loginResponse = await agent
-    .post("/api/users/login")
-    .send({ email, password });
-
-  if (loginResponse.status !== 200) {
-    throw new Error("Login failed during test setup");
-  }
-
-  const match = loginResponse.headers["set-cookie"][0].match(/jwt=([^;]+)/);
-  if (!match) {
-    throw new Error("Failed to extract token from cookie");
-  }
-
-  return match[1];
-};
-
-export const uploadImageAndGetPath = async (
-  agent: SuperAgentTest,
-  filePath: string
-): Promise<string> => {
-  const file = fs.createReadStream(filePath);
-  const fileName = path.basename(filePath); // Extract the file name from the path
-  const response = await agent
-    .post("/api/upload")
-    .attach("image", file, fileName);
-
-  expect(response.status).toBe(200);
-  expect(response.body).toEqual({
-    message: "Image uploaded successfully",
-    image: expect.stringContaining("/frontend\\public\\images\\image-"),
-  });
-
-  return response.body.image;
-};
-
-export const createProduct = async (userId: number): Promise<Product> => {
-  return await db.product.create({
-    data: {
-      userId, // add the userId to the data
-      name: "Test Product",
-      price: 100,
-      image: "sample path",
-      brand: "Test Brand",
-      category: "Test Category",
-      countInStock: 10,
-      numReviews: 0,
-      description: "Test Description",
     },
   });
 };
@@ -145,4 +107,45 @@ export const createAdminUserAndRetrieve = async (
   }
 
   return user;
+};
+
+// Other Operations
+export const loginUserAndGetToken = async (
+  agent: SuperAgentTest,
+  email: string,
+  password: string
+): Promise<string> => {
+  const loginResponse = await agent
+    .post("/api/users/login")
+    .send({ email, password });
+
+  if (loginResponse.status !== 200) {
+    throw new Error("Login failed during test setup");
+  }
+
+  const match = loginResponse.headers["set-cookie"][0].match(/jwt=([^;]+)/);
+  if (!match) {
+    throw new Error("Failed to extract token from cookie");
+  }
+
+  return match[1];
+};
+
+export const uploadImageAndGetPath = async (
+  agent: SuperAgentTest,
+  filePath: string
+): Promise<string> => {
+  const file = fs.createReadStream(filePath);
+  const fileName = path.basename(filePath);
+  const response = await agent
+    .post("/api/upload")
+    .attach("image", file, fileName);
+
+  expect(response.status).toBe(200);
+  expect(response.body).toEqual({
+    message: "Image uploaded successfully",
+    image: expect.stringContaining("/frontend\\public\\images\\image-"),
+  });
+
+  return response.body.image;
 };
