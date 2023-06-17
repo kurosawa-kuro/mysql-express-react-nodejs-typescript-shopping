9b65f2c00a22b18310caf1f0a9cbd03cc4d8b4da
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-06-17 09:58:47 +0900

    add test-utils

--- backend/__test__/admin/createProduct.test.ts
+++ backend/__test__/admin/createProduct.test.ts
@@ -1,69 +1,12 @@
-import request, { SuperAgentTest } from "supertest";
-import bcrypt from "bcryptjs";
+import request from "supertest";
 import path from "path";
-import fs from "fs";
 import { app } from "../../index";
-import { db } from "../../database/prisma/prismaClient";
-
-const createAdminUser = async (
-  email: string,
-  password: string
-): Promise<void> => {
-  const hashedPassword = await bcrypt.hash(password, 10);
-  await db.user.create({
-    data: {
-      name: "Admin",
-      email,
-      password: hashedPassword,
-      isAdmin: true,
-    },
-  });
-};
-
-const loginUserAndGetToken = async (
-  agent: SuperAgentTest,
-  email: string,
-  password: string
-): Promise<string> => {
-  const loginResponse = await agent
-    .post("/api/users/login")
-    .send({ email, password });
-
-  if (loginResponse.status !== 200) {
-    throw new Error("Login failed during test setup");
-  }
-
-  const match = loginResponse.headers["set-cookie"][0].match(/jwt=([^;]+)/);
-  if (!match) {
-    throw new Error("Failed to extract token from cookie");
-  }
-
-  return match[1];
-};
-
-const uploadImageAndGetPath = async (
-  agent: SuperAgentTest,
-  filePath: string,
-  fileName: string
-): Promise<string> => {
-  const file = fs.createReadStream(filePath);
-  const response = await agent
-    .post("/api/upload")
-    .attach("image", file, fileName);
-
-  expect(response.status).toBe(200);
-  expect(response.body).toEqual({
-    message: "Image uploaded successfully",
-    image: expect.stringContaining("/frontend\\public\\images\\image-"),
-  });
-
-  return response.body.image;
-};
-
-const clearDatabase = async (): Promise<void> => {
-  await db.product.deleteMany();
-  await db.user.deleteMany();
-};
+import {
+  createAdminUser,
+  loginUserAndGetToken,
+  uploadImageAndGetPath,
+  clearDatabase,
+} from "../test-utils";
 
 describe("Product creation", () => {
   const agent = request.agent(app);
---
+++ backend/__test__/test-utils.ts
@@ -0,0 +1,65 @@
+import { SuperAgentTest } from "supertest";
+import bcrypt from "bcryptjs";
+import path from "path";
+import fs from "fs";
+import { db } from "../database/prisma/prismaClient";
+
+export const createAdminUser = async (
+  email: string,
+  password: string
+): Promise<void> => {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  await db.user.create({
+    data: {
+      name: "Admin",
+      email,
+      password: hashedPassword,
+      isAdmin: true,
+    },
+  });
+};
+
+export const loginUserAndGetToken = async (
+  agent: SuperAgentTest,
+  email: string,
+  password: string
+): Promise<string> => {
+  const loginResponse = await agent
+    .post("/api/users/login")
+    .send({ email, password });
+
+  if (loginResponse.status !== 200) {
+    throw new Error("Login failed during test setup");
+  }
+
+  const match = loginResponse.headers["set-cookie"][0].match(/jwt=([^;]+)/);
+  if (!match) {
+    throw new Error("Failed to extract token from cookie");
+  }
+
+  return match[1];
+};
+
+export const uploadImageAndGetPath = async (
+  agent: SuperAgentTest,
+  filePath: string,
+  fileName: string
+): Promise<string> => {
+  const file = fs.createReadStream(filePath);
+  const response = await agent
+    .post("/api/upload")
+    .attach("image", file, fileName);
+
+  expect(response.status).toBe(200);
+  expect(response.body).toEqual({
+    message: "Image uploaded successfully",
+    image: expect.stringContaining("/frontend\\public\\images\\image-"),
+  });
+
+  return response.body.image;
+};
+
+export const clearDatabase = async (): Promise<void> => {
+  await db.product.deleteMany();
+  await db.user.deleteMany();
+};
