a42c3f1498b7bb92be5eb7bc35da6cad2bf18efa
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-06-25 17:51:10 +0900

    fix renderHook

--- frontend/src/__test__/product/ProductScreenLoggedIn.test.tsx
+++ frontend/src/__test__/product/ProductScreenLoggedIn.test.tsx
@@ -1,24 +1,13 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen, renderHook } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
 import { App } from "../../App";
 import { LoginScreen } from "../../screens/auth/LoginScreen";
-import { createServer, inputField, TEST_USER } from "../test-utils";
+import { createServer, printDOM } from "../test-utils";
 import { HomeScreen } from "../../screens/product/HomeScreen";
-import { product } from "../mocks";
+import { useAuthStore } from "../../state/store";
 
 const server = createServer();
-
-const LABELS = {
-  email: "email",
-  password: "password",
-  name: "Name",
-  price: "Price",
-  imageFile: "Image File",
-  brand: "Brand",
-  countInStock: "Count In Stock",
-  category: "Category",
-  description: "Description",
-};
 
 describe("Product Operation", () => {
   beforeAll(() => server.listen());
@@ -26,9 +15,9 @@ describe("Product Operation", () => {
   afterAll(() => server.close());
 
   describe("User", () => {
-    beforeEach(async () => {
+    it("should display user info after login", async () => {
       render(
-        <MemoryRouter initialEntries={["/login"]}>
+        <MemoryRouter initialEntries={["/"]}>
           <Routes>
             <Route path="/" element={<App />}>
               <Route path="/login" element={<LoginScreen />} />
@@ -39,49 +28,26 @@ describe("Product Operation", () => {
         </MemoryRouter>
       );
 
-      // Enter login credentials
-      inputField(LABELS.email, TEST_USER.email);
-      inputField(LABELS.password, TEST_USER.password);
-      fireEvent.click(screen.getByTestId("login"));
+      // 強制的にログインさせる
+      const { result } = renderHook(() => useAuthStore());
 
-      // Wait for login to complete
-      await screen.findByText("Successfully logged in");
-    });
-
-    it("can log in", async () => {
-      // Verify successful login
-      const toastMessage = await screen.findByText("Successfully logged in");
-      expect(toastMessage).toBeInTheDocument();
-    });
-
-    it("should display user info after login", async () => {
-      // Verify successful login icon
-      const toastMessage = await screen.findByText("Successfully logged in");
-      const toastContainer = toastMessage.parentElement;
-      const successIcon = toastContainer?.querySelector("svg");
-      expect(successIcon).toBeInTheDocument();
+      // Simulate login by setting user information
+      act(() => {
+        result.current.setUserInformation({
+          id: 1,
+          name: "Test User",
+          email: "test@example.com",
+          isAdmin: false,
+          token: "testToken",
+        });
+      });
 
       // Verify user information display after successful login
-      await screen.findByText(TEST_USER.name, {
+      await screen.findByText("Test User", {
         selector: '[data-testid="user-info-name"]',
       });
-    });
 
-    it("should display products after login", async () => {
-      // Verify 'Latest Products' and product name display after successful login
-      expect(await screen.findByText("Latest Products")).toBeInTheDocument();
-      expect(await screen.findByText(product.name)).toBeInTheDocument();
-    });
-
-    it("can perform a product search", async () => {
-      // Perform a product search
-      const searchInput = screen.getByPlaceholderText("Search Products...");
-      fireEvent.change(searchInput, { target: { value: "Product 1" } });
-      const searchButton = screen.getByText("Search");
-      fireEvent.click(searchButton);
-      expect(await screen.findByText(product.name)).toBeInTheDocument();
-
-      // printDOM();
+      printDOM();
     });
   });
 });
