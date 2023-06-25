a4aed76378b0754e22e63a3d0191c19adcb740bc
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-06-26 02:26:04 +0900

    fix error test

---
+++ backend/__test__/notFound.test.ts
@@ -0,0 +1,29 @@
+// External Imports
+import express from "express";
+import request from "supertest";
+import { notFound, errorHandler } from "../middleware/errorMiddleware";
+
+describe("Error Middlewares", () => {
+  let app: express.Application;
+
+  beforeEach(() => {
+    // Set up express app and apply middlewares
+    app = express();
+    app.use(express.json());
+    app.use("/test", (req, res) => {
+      res.json({ message: "Test route" });
+    });
+    app.use(notFound);
+    app.use(errorHandler);
+  });
+
+  it("should return 404 error for not found route", async () => {
+    const res = await request(app).get("/non-existing-route");
+
+    expect(res.status).toBe(404);
+    expect(res.body).toEqual({
+      message: "Not Found - /non-existing-route",
+      stack: expect.any(String),
+    });
+  });
+});
--- backend/middleware/errorMiddleware.ts
+++ backend/middleware/errorMiddleware.ts
@@ -18,16 +18,8 @@ const errorHandler = (
   next: NextFunction
 ): void => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-
-  // If Prisma not found error, set to 404 and change message
-  const message = err.code === "P2025" ? "Resource not found" : err.message;
-
-  if (err.code === "P2025") {
-    res.status(404);
-  }
-
   res.status(statusCode).json({
-    message,
+    message: err.message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 };
