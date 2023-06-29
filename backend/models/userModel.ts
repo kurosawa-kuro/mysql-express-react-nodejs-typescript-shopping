17f29eff09b5a15fd4285acf9ff138a842ff1c44
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-06-29 21:49:57 +0900

    fix order

--- backend/models/userModel.ts
+++ backend/models/userModel.ts
@@ -4,24 +4,24 @@ import { db } from "../database/prisma/prismaClient";
 import bcrypt from "bcryptjs";
 import { Prisma } from "@prisma/client";
 
+export const createUserInDB = async (data: Prisma.UserCreateInput) => {
+  return await db.user.create({ data });
+};
+
 export const readUserByEmailInDB = async (email: string) => {
   return await db.user.findUnique({ where: { email } });
+};
+
+export const readAllUsersFromDB = async () => {
+  return await db.user.findMany();
 };
 
 export const readUserByIdInDB = async (id: number) => {
   return await db.user.findUnique({ where: { id } });
 };
 
-export const createUserInDB = async (data: Prisma.UserCreateInput) => {
-  return await db.user.create({ data });
-};
-
 export const updateUserByIdInDB = async (id: number, data: any) => {
   return await db.user.update({ where: { id }, data });
-};
-
-export const readAllUsersFromDB = async () => {
-  return await db.user.findMany();
 };
 
 export const deleteUserByIdInDB = async (id: number) => {
